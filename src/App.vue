43ceae5fb7225aa2a2658be4919a1df28b8e3b68
Elmeddin Xudaverdiyev <Xudaverdiyev11@gmail.com>
2021-11-16 11:32:39 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Navbar/>
+    <Index/>
+    <Footer/>
+</template>
+
+<script>
+import Navbar from "./views/allviews/Navbar.vue";
+import Footer from "./views/allviews/Footer.vue";
+import Index from "./components/home/Index.vue";
+export default {
+  components: {
+    Navbar,
+    Footer,
+    Index
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,57 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import "./assets/fonts/Poppins-Bold.woff"
+// import "./assets/fonts/Poppins-Light.woff"
+// import "./assets/fonts/Poppins-Regular.woff"
+// import "./assets/fonts/Poppins-SemiBold.woff"
+
+// import "./assets/fonts/fa-brands-400.eot"
+// import "./assets/fonts/fa-brands-400.svg"
+// import "./assets/fonts/fa-brands-400.woff"
+// import "./assets/fonts/fa-brands-400.woff2"
+
+// import "./assets/fonts/fa-regular-400.eot"
+// import "./assets/fonts/fa-regular-400.svg"
+// import "./assets/fonts/fa-regular-400.ttf"
+// import "./assets/fonts/fa-regular-400.woff"
+// import "./assets/fonts/fa-regular-400.woff2"
+
+// import "./assets/fonts/fa-solid-900.eot"
+// import "./assets/fonts/fa-solid-900.svg"
+// import "./assets/fonts/fa-solid-900.ttf"
+// import "./assets/fonts/fa-solid-900.woff"
+// import "./assets/fonts/fa-solid-900.woff2"
+
+// import "./assets/fonts/icofont.woff"
+// import "./assets/fonts/icofont.woff2"
+
+import "./assets/css/animate.css"
+import "./assets/css/bootstrap-datepicker.min.css"
+import "./assets/css/bootstrap.min.css"
+import "./assets/css/fontawesome.min.css"
+import "./assets/css/hover-min.css"
+import "./assets/css/icofont.css"
+import "./assets/css/magnific-popup.css"
+import "./assets/css/responsive.css"
+import "./assets/css/slick-theme.css"
+import "./assets/css/slick.css"
+import "./assets/css/swiper.min.css"
+import "./assets/css/style.css"
+
+import "JQuery"
+import "magnific-popup"
+import "popper.js"
+import "bootstrap"
+import "bootstrap-datepicker"
+import "./assets/js/sidebar"
+import "slick-animation"
+// import "./assets/js/contact"
+import "./assets/js/swiper-custom"
+// import "./assets/js/main"
+
+
+
+
+
+createApp(App).mount('#app')
